9172f294a400637869f7f2c8bebaebf15107adc3
front-hendy <hendy.almeida@hotmail.com>
2019-11-23 10:13:39 -0300

    add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require_once('header.php'); ?>
+<main>
+  <div class="container my-5">
+    <h3>Produtos</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Categoria</th>
+          <th>Status</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Halloween box</td>
+          <td>R$45,00</td>
+          <td>Temática</td>
+          <td>Ativo</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</main>
+
+<?php require_once('footer.php'); ?>
