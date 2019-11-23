f4116fa71f7d42f6e0c59e70465a0f1ebd5a645f
front-hendy <hendy.almeida@hotmail.com>
2019-11-23 10:03:22 -0300

    front login

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 include_once('database.php');
 
-echo "entrei no functions";
+// echo "entrei no functions";
 // var_dump($connection);
 
 $usuarios = $connection->query('SELECT * FROM usuario');
 $usuarios = $usuarios->fetchAll();
 
-echo "<pre>";
-var_dump($usuarios);
+// echo "<pre>";
+// var_dump($usuarios);
--- login.php
+++ login.php
@@ -7,10 +7,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>PirateBox - Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 
 <body>
+  <header class="text-center">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="imagens/piratebox-logo.png" width="200" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">Home</a>
+          <a class="nav-item nav-link" href="#">Produtos</a>
+          <a class="nav-item nav-link" href="#">Pedidos</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-5">
+          <h1>Login</h1>
+          <form method="POST" class="my-5">
+            <div class="form-group">
+              <label for="email">email</label>
+              <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="senha">senha</label>
+              <input type="password" name="senha" id="senha" class="form-control">
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary">Entrar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </main>
 
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
